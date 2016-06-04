937e71bc3a8ff6200df782efe65c166cc9deb839
nsato <nobuosato@gmail.com>
2016-06-04 13:15:11 -0400

    ns update

--- analysis/parameters.py
+++ analysis/parameters.py
@@ -3,6 +3,7 @@ import sys,os
 import numpy as np
 from numpy.random import uniform
 from tools import save,load,checkdir
+from numpy import linalg as LA
 ##########################################
 import matplotlib.gridspec as gridspec
 import matplotlib as mpl
@@ -13,7 +14,7 @@ rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text',usetex=True)
 ##########################################
 
-class _HESS(object):
+class dep_HESS(object):
 
   def __init__(self):
 
@@ -340,14 +341,22 @@ class HESS(object):
     order.append('bd_T')
     self.order=order
 
+    self.P0 =np.array([par[o]['val'] for o in order])
+    self.dP0=np.array([par[o]['err'] for o in order])
+
   def _gen_hess_pars(self,hi,hj,Ki,Kj):
     par=self.par
     order=self.order
     L=''
+    flag=False
     for o in order:
       if o==Ki:   L+='%15.5e'%(par[o]['val']+hi)
       elif o==Kj: L+='%15.5e'%(par[o]['val']+hj)
       else:       L+='%15.5e'%par[o]['val']
+      if o=='bu_T' or o=='bd_T':
+        if par[o]['val']+hi<0 and flag==False:
+          self.cnt+=1
+          flag=True
     return L
 
   def _get_target_par(self,Ki,Kj,All=False):
@@ -371,37 +380,216 @@ class HESS(object):
     npar=len(order)
     L=[]
     L.append('        %s'%self._get_target_par(None,None,All=True))
+    self.cnt=0
+    L.append('----- ----- %s'%(self._gen_hess_pars(0,0,'-','-')))
     for i in range(npar):
       for j in range(i,npar):
+          pi=order[i]
+          pj=order[j]
+          hi=par[pi]['err']
+          hj=par[pj]['err']
 
-        pi=order[i]
-        pj=order[j]
-        hi=par[pi]['err']
-        hj=par[pj]['err']
+          #L.append('='*100)
+          #L.append('h(%s)=%0.2e  h(%s)=%0.2e'%(pi,hi,pj,hj))
+          #L.append('        %s'%self._get_target_par(pi,pj))
 
-        #L.append('='*100)
-        #L.append('h(%s)=%0.2e  h(%s)=%0.2e'%(pi,hi,pj,hj))
-        #L.append('        %s'%self._get_target_par(pi,pj))
+          if i!=j:
+            L.append('+%s +%s %s'%(pi,pj,self._gen_hess_pars(hi,hj,pi,pj)))
+            L.append('+%s -%s %s'%(pi,pj,self._gen_hess_pars(hi,-hj,pi,pj)))
+            L.append('-%s +%s %s'%(pi,pj,self._gen_hess_pars(-hi,hj,pi,pj)))
+            L.append('-%s -%s %s'%(pi,pj,self._gen_hess_pars(-hi,-hj,pi,pj)))
+          else:
+            L.append('+%s  %s %s'%(pi,pj,self._gen_hess_pars(hi,0,pi,pj)))
+            L.append('-%s  %s %s'%(pi,pj,self._gen_hess_pars(-hi,0,pi,pj)))
 
-        L.append('+%s +%s %s'%(pi,pj,self._gen_hess_pars(hi,hj,pi,pj)))
-        L.append('+%s -%s %s'%(pi,pj,self._gen_hess_pars(hi,-hj,pi,pj)))
-        L.append('-%s +%s %s'%(pi,pj,self._gen_hess_pars(-hi,hj,pi,pj)))
-        L.append('-%s -%s %s'%(pi,pj,self._gen_hess_pars(-hi,-hj,pi,pj)))
-
+    print self.cnt
     L=[l+'\n' for l in L]
     checkdir('data')
     F=open('data/hessgrid.dat','w')
     F.writelines(L)
     F.close()
 
+  def get_old_hessian(self):
+    F=open('data/correlation_matrix.dat')
+    L=F.readlines()
+    F.close()
+    L=[l.strip().split() for l in L if l.strip()!='']
+    L=[[float(x) for x in l] for l in L]
+    corr=np.array(L)
+    ndim=len(L)
+    cov=np.zeros((ndim,ndim))
+    for i in range(ndim):
+      for j in range(ndim):
+        cov[i,j]=corr[i,j]*self.dP0[i]*self.dP0[j]
+    hess=LA.inv(cov)
+    save(hess,'data/old.hess')
+
+  def get_new_hessian(self,fname):
+    path='hessian_collins/hessian_results/'
+
+    # get off diagonal parts
+    F=open(path+fname+'.out')
+    L=F.readlines()
+    F.close()
+    L=[l.strip().split() for l in L] 
+
+    # construct dict as
+    # D[par1,par2] = {'P,P':#,'P,M':#,...}
+    D={}
+    for l in L:
+      p1,p2=l[:2]
+      key='%s,%s'%(p1.replace('M_','').replace('P_',''),p2.replace('M_','').replace('P_',''))
+      if key not in D: D[key]={}
+      key2='%s,%s'%(p1.split('_')[0],p2.split('_')[0])
+      if float(l[-1])>0:
+        D[key][key2]=float(l[-1])
+      else:
+        D[key][key2]=None
+
+    # get diagonal parts
+    F=open(path+fname+'.diag')
+    L=F.readlines()
+    F.close()
+    L=[l.strip().split() for l in L] 
+
+    for l in L:
+      p1,p2=l[:2]
+      key='%s,%s'%(p1.replace('M_','').replace('P_',''),p2.replace('M_','').replace('P_',''))
+      if key=='FIRST,FIRST': 
+        D['central']=float(l[-1])
+        continue
+      if key not in D: D[key]={}
+      key2='%s,0'%(p1.split('_')[0])
+      if float(l[-1])>0:
+        D[key][key2]=float(l[-1])
+      else:
+        D[key][key2]=None
+
+
+    # construct hessian
+    npar=len(self.order)
+    H=np.zeros((npar,npar))
+    for k in D:
+      if k=='central': continue
+      #print k
+      #continue
+      #for kk in D[k]:
+      #  print '\t',kk,D[k][kk]
+
+      # get target entry at hess matrix (I,J)
+      p1,p2=k.split(',')
+      for i in range(npar): 
+        if self.order[i]==p1: I=i
+        if self.order[i]==p2: J=i
+
+      # step size for target entry
+      hi=self.par[p1]['err']
+      hj=self.par[p2]['err']
+
+      # compute hess entry
+      if all([x!=None for x in [D[k][kk] for kk in ['P,P','M,M','P,M','M,P']]]):
+        #print D[k]['P,P']
+        #print D[k]['M,M']
+        #print D[k]['P,M']
+        #print D[k]['M,P']
+        #print (4*hi*hj)
+        if I!=J: 
+          H[I,J]=(D[k]['P,P']+D[k]['M,M']-D[k]['P,M']-D[k]['M,P'])/(4*hi*hj)
+        elif I==J:
+          H[I,J]=(D[k]['P,0']+D[k]['M,0']-2*D['central'])/(hi**2)
+      else:
+        H[I,J]=None
+
+    for i in range(npar):
+      for j in range(npar):
+        if i<j: H[j,i]=H[i,j]
+
+    # get final hessian = 1/2 dchi2/dxdy
+    H*=0.5
+
+
+    # print hessian
+    row='%10s'%''
+    for i in range(npar): row+='%10s'%self.order[i]
+    print row
+    for i in range(npar):
+      row='%10s  '%self.order[i]
+      for j in range(npar): 
+        if np.isnan(H[i,j])==False:
+          row+='%10.2e'%H[i,j]
+        else:
+          row+='%10s'%''
+      print row
+
+    # save hessian
+    save(H,'data/'+fname+'.hess')
+
+  def tmp(self,fname):
+    H0=load('data/old.hess')
+    H=load('data/'+fname+'.hess')
+    ndim=H0.shape[0]
+    I=[i for i in range(ndim) if np.isnan(H[0][i])==False]
+    nrdim=len(I)
+
+    RH0=np.copy(H0[I,:][:,I])
+    RH=np.copy(H[I,:][:,I])
+
+    P0=self.P0[I]
+    SAMPLE0 = np.random.multivariate_normal(P0,LA.inv(RH0),10000).T
+    SAMPLE  = np.random.multivariate_normal(P0,LA.inv(RH0+RH),10000).T
+    names=[self.order[i] for i in I]
+
+    ncols,nrows=3,4
+    py.figure(figsize=(ncols*5,nrows*3))
+    for i in range(nrdim):
+      print i
+      ax=py.subplot(nrows,ncols,i+1)
+      Range=None
+      bins=100
+      ax.hist(SAMPLE0[i],histtype='stepfilled',range=Range,color='Yellow',bins=bins)
+      ax.hist(SAMPLE[i],histtype='stepfilled',range=Range,color='b',bins=bins)
+
+      pmap={}
+      pmap['BLNY']=r'$BLNY$'
+      pmap['Nu_c']=r'$N_{u(c)}$'
+      pmap['Nd_c']=r'$N_{d(c)}$'
+      pmap['au_c']=r'$a_{u(c)}$'
+      pmap['ad_c']=r'$a_{d(c)}$'
+      pmap['bu_c']=r'$b_{u(c)}$'
+      pmap['bd_c']=r'$b_{d(c)}$'
+      pmap['Nu_T']=r'$N_{u(T)}$'
+      pmap['Nd_T']=r'$N_{d(T)}$'
+      pmap['au_T']=r'$a_{u(T)}$'
+      pmap['ad_T']=r'$a_{d(T)}$'
+      pmap['bu_T']=r'$b_{u(T)}$'
+      pmap['bd_T']=r'$b_{d(T)}$'
+
+      ax.text(0.1,0.8,pmap[names[i]],transform=ax.transAxes,size=20)
+      #ax.legend(frameon=0,fontsize=20,loc='best')
+      #ax.tick_params(axis='both', which='major', labelsize=20)
+    py.tight_layout()
+    py.savefig('gallery/%s.pdf'%fname)
+
+
+
 if __name__=='__main__':
 
   hess=HESS()
-  hess.get_hess_grid()
+  #hess.get_hess_grid()
+  #hess.get_new_hessian('clas_p_pim')
+  #hess.get_new_hessian('clas_p_pip')
+  #hess.get_new_hessian('solid_3he_pim')
+  #hess.get_new_hessian('solid_3he_pip')
+  #hess.get_new_hessian('solid_p_pim')
+  #hess.get_new_hessian('solid_p_pip')
+  #hess.get_old_hessian()
 
-
-
-
+  #hess.tmp('clas_p_pim')
+  #hess.tmp('clas_p_pip')
+  #hess.tmp('solid_3he_pim')
+  #hess.tmp('solid_3he_pip')
+  #hess.tmp('solid_p_pim')
+  hess.tmp('solid_p_pip')
 
 
 
